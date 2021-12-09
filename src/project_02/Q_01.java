eeac13b99ac90355f15b7eb572e6513b92d35d1e
EmrhSGLM <emrh.sglm7168@gmail.com>
2021-12-09 12:35:07 +0300

    question solution

---
+++ src/project_02/Q_01.java
@@ -0,0 +1,34 @@
+package project_02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Q_01 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Soru 8 :***** EMRAH BEY *****
+		 * 
+		 * Bir int array verildiğinde, array uzunluğu 1 veya daha fazlaysa ve ilk öğe
+		 * ile son öğe eşitse true değerini yazdırın.
+		 * 
+		 * sameFirstLast([1, 2, 3]) → false 
+		 * sameFirstLast([1, 2, 3, 1]) → true
+		 * sameFirstLast([1, 2, 1]) → true 
+		 * sameFirstLast([1]) → true
+		 * 
+		 * sadece true veya false print et
+		 */
+		int arr[] = {1};
+				
+		if (arr.length >= 1 && arr[0] == arr[arr.length-1]) {
+			
+			System.out.println(Arrays.toString(arr)+" ==> " + "true");
+		} else System.out.println(Arrays.toString(arr)+" ==> " + "false");
+		
+
+	}
+
+}
